33f5cbdf91f2adfa7e73d85fc55131d0325f8fb3
AnjaliTestYantra <anjali.paleth@gmail.com>
2024-07-24 17:34:42 +0530

    second commit

---
+++ src/main/java/genericUtility/BaseClass.java
@@ -0,0 +1,11 @@
+package genericUtility;
+
+import org.testng.annotations.Test;
+
+public class BaseClass {
+
+	@Test
+	public void test() {
+		System.out.println("demo 1");
+	}
+}
---
+++ src/test/java/practice/ToLearnGit.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class ToLearnGit {
+
+	@Test
+	public void git() {
+		System.out.println("Demo Git");
+	}
+}
